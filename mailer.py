5ab8021138072b16431d403d6ebe90004b172a4b
Chhavi Sharma <sharma.chhavi96@gmail.com>
2017-08-27 21:42:15 +0530

    Add files via upload

---
+++ mailer.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import sendgrid
+from sendgrid.helpers.mail import *
+import logging        
+from logging.handlers import RotatingFileHandler
+
+def difInWebsite(first, second):
+	#we will come here later
+
+
+if __name__ == '__main__':
+	webpage = requests.get('')
